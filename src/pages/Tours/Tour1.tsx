5cde8daaced5a0127816c3878bf904dcb105748f
pranali2259 <pranali.k@techouts.com>
2024-11-26 16:44:33 +0530

    Update Tour1.tsx

--- src/pages/Tours/Tour1.tsx
+++ src/pages/Tours/Tour1.tsx
@@ -1,79 +1,4 @@
-// import * as React from 'react';
-// import CartData from '@/pages/cart/Cart.json';  
-// import Link from 'next/link';
-// import Slider from "react-slick";
-// interface CartItem {
-//   id: number;
-//   title: string;
-//   discreption: string;  
-//   img: string;
-// }
- 
-// const carts = () => {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     speed: 2000,
-//     autoplaySpeed: 2000,
-//     cssEase: "linear"
-//   };
-//   const [TourData, setTourData] = React.useState<CartItem[]>([]);  
 
-//   React.useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch("http://localhost:5000/Tours");
-
-         
-//         if (!response.ok) {
-//           throw new Error("Failed to fetch data");
-//         }
-
-         
-//         const data: CartItem[] = await response.json();  
-//         setTourData(data);  
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-//   return (
-    
-//     <div className="py-8">
-    
-//     <div className="text-center font-bold text-black text-2xl mb-8">
-//     <Link href={'/Tours/Tour'}> Explore the India</Link> 
-//     </div>
-  
-//     <div className="flex flex-wrap gap-8 justify-center">
-//       {TourData.slice(0, 4).map((tour, index) => (
-//         <div key={index} className="max-w-sm w-full bg-white shadow-lg rounded-lg overflow-hidden">
-//           <img
-//             src={tour.img}
-//             alt="Blog Image"
-//             className="w-full h-64 object-cover rounded-t-lg"
-//           />
-  
-//           <div className="p-4">
-//             <p className="text-gray-800 font-semibold text-lg mb-4">
-//             <Link href={`/Tours/${tour.id}`}> {tour.title}</Link>
-//             </p>
-           
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   </div>
-  
-//   )
-// };
-
-// export default carts;
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css"; 
 import * as React from 'react';
@@ -94,7 +19,7 @@ const Carts = () => {
     speed: 1000,
     autoplay: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     autoplaySpeed: 1500,
   };
 
